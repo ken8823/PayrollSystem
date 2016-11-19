cdb0d5329930d56daf73c30d114d175ffdcc620f
Scott Graham <sukohamu@gmail.com>
2016-11-18 19:01:13 -0500

    Added createcompany and inviteuser commands

--- Payroll.py
+++ Payroll.py
@@ -22,18 +22,28 @@ def startProgram():
 	"""
 	class description
 	"""	
+	company = None
+	
 	print('\nWelcome to the Payroll sytem!' )
-	print('\nHere are some useful commands: \ncreatecompany\ninviteuser\nexit\n' )
-
+	print('\nHere are some useful commands: \ncreatecompany [name]\ninviteemployee [email address]\nviewemployees\nexit\n' )
+	
 	while True:
-		userinput = input('Please enter a command:')
+		userinput = input('\nPlease enter a command:')
 		
-		if userinput.lower() == 'createcompany':
-			print('Company created')
-		elif userinput.lower() == 'inviteuser':
-			print('User Invited')
-		elif userinput.lower() == 'exit':
-			print('Goodbye!')
+		userinput = userinput.split(' ', 1)
+		
+		if userinput[0].lower() == 'createcompany':
+			company = Company(userinput[1])
+			print('Company ', company.getName(), ' has been created')
+		elif userinput[0].lower() == 'inviteemployee':
+			company.addEmployee(userinput[1])
+			print(userinput[1], ' was invited to the company!')
+		elif userinput[0].lower() == 'viewemployees':
+			print()
+		elif userinput[0].lower() == 'editemployee':
+			print()
+		elif userinput[0].lower() == 'exit':
+			print('\nGoodbye!')
 			sys.exit()
 		else:
 			print('ERROR: Command does not exist')
@@ -45,25 +55,40 @@ class Company:
 	
 	def __init__(self, name):
 		self.name = name
+		self.userList = UserList()
+	
+	def getName(self):
+		return self.name
+		
+	def addEmployee(self, email):
+		newUser = User(email)
+		self.userList.addUser(newUser)
+	
+	def viewEmployees(self):
+		return self.userList
+		
 
 class UserList:
 	"""
 	class description
 	"""
 	
-	#def addUser():
+	def __init__(self):
+		self.userList = []
 		
-	#def getUsers():
+	def addUser(self, user):
+		self.userList.append(user)
+		
+	def getUsers(self):
+		return userList
 		
 class User:
 	"""
 	User class description
 	"""	
-	def __init__(self, name):
-		self.name = name
-		
-	#def getUser(user):
-		
+	def __init__(self, email, name="Unknown"):
+		self.email = email
+		self.name = name		
 	
 if __name__ == "__main__":
 	username = input('Enter Username: ') #temp username is admin
